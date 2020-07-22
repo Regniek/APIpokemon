5a0c42269680d30cca1c4066e91b42131c968b0b
Juan Espitia <juanespitia@outlook.com>
2020-07-21 23:48:54 -0500

    Convertimos busquedas en arreglos y comparamos

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const pokedex = require('pokedex-promise-v2')
 
 var P = new pokedex();
 
-P.getContestTypeByName('poison')
+let pokemones = [];
+
+P.getTypeByName("poison")
 .then(function(response) {
-  console.log(response);
+  const poisonpokes = response.pokemon
+  const poisontotal = Object.keys(response.pokemon).length
+  console.log(poisontotal,poisonpokes);
+  
 })
 .catch(function(error) {
   console.log('There was an ERROR: ', error);
 });
+
+
+// P.getGenerationByName("generation-1")
+// .then(function(response) {
+//   console.log(response.length);
+// })
+// .catch(function(error) {
+//   console.log('There was an ERROR: ', error);
+// });
